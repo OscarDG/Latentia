07be517e101d23ab8f3edaec83f07cc93085620a
Oscar Guerrero <oscard.guerrerom@outlook.com>
2026-04-28 14:09:41 -0500

    feat: restructure project layout and add header component
    - Refactored the main page to use a new MainLayout component.
    - Created a Header component for navigation with responsive design.
    - Added links data structure for navigation items.
    - Introduced global CSS styles with a color theme.
    - Implemented new pages: Nosotros, Empresas, Personas, and Contacto.
    - Updated TypeScript configuration for JSX support.

---
+++ src/data/links.ts
@@ -0,0 +1,20 @@
+import type { Link } from "../types/link_types.ts";
+
+export const links: Link[] = [
+    {
+        label: "Inicio",
+        href: "/",
+    },
+    {
+        label: "Nosotros",
+        href: "/nosotros",
+    },
+    {
+        label: "Servicios",
+        href: "/servicios",
+    },
+    {
+        label: "Contacto",
+        href: "/contacto",
+    }
+];
